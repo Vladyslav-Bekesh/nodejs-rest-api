87faba2eb30451ea34129898527f4b03d094ef60
Vladyslav <dyadyavadya308@gmail.com>
2023-08-23 20:20:21 +0300

    features/now only contacts owner can change contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,6 +3,10 @@ const express = require("express");
 const { validateBody } = require("../../middlewares/validateBody");
 const { isValidId } = require("../../middlewares/isValidId");
 const { authenticate } = require("../../middlewares/authenticate");
+const {
+  checkContactOwnership,
+} = require("../../middlewares/checkContactOwnership");
+
 const { addSchema, updateSchema } = require("../../models/contact");
 
 const {
@@ -18,14 +22,33 @@ const router = express.Router();
 
 router.get("/", authenticate, getAll);
 
-router.get("/:contactId", authenticate, getById);
+router.get("/:contactId", authenticate, checkContactOwnership, getById);
 
 router.post("/", authenticate, validateBody(addSchema), add);
 
-router.put("/:contactId", authenticate, isValidId, validateBody(updateSchema), updateById);
+router.put(
+  "/:contactId",
+  authenticate,
+  checkContactOwnership,
+  isValidId,
+  validateBody(updateSchema),
+  updateById
+);
 
-router.delete("/:contactId", authenticate, isValidId, deleteById);
+router.delete(
+  "/:contactId",
+  authenticate,
+  checkContactOwnership,
+  isValidId,
+  deleteById
+);
 
-router.patch("/:contactId", authenticate, isValidId, updateFavoriteStatus);
+router.patch(
+  "/:contactId",
+  authenticate,
+  checkContactOwnership,
+  isValidId,
+  updateFavoriteStatus
+);
 
 module.exports = router;
